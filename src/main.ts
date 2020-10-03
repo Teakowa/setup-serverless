df55ba350f13777dd98cf8432ea876189ede7478
Teakowa <hub+git@teakowa.dev>
2020-10-03 16:12:25 +0800

    refactor(main): using env variable instead of inputs

--- src/main.ts
+++ src/main.ts
@@ -8,46 +8,57 @@ import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as io from '@actions/io';
 
-async function useProvider(
-  provider: string,
-  secretId: string,
-  secretKey: string
-) {
+async function useProvider(provider: string) {
   switch (provider) {
     case 'aws': {
+      if (
+        !process.env['AWS_ACCESS_KEY_ID'] ||
+        !process.env['AWS_SECRET_ACCESS_KEY']
+      ) {
+        core.setFailed(
+          'Missing aws required environment variables: AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY'
+        );
+      }
       break;
     }
     case 'tencent': {
-      const accountId = core.getInput('tencent_appid');
-      if (!accountId) {
-        throw new Error('Missing required arguments: ' + 'tencent_appid');
+      const appid = process.env['TENCENT_APPID'];
+      const secretId = process.env['TENCENT_SECRET_ID'];
+      const secretKey = process.env['TENCENT_SECRET_KEY'];
+
+      if (!appid || !secretId || !secretKey) {
+        core.setFailed('Missing tencent required environment variables.');
       }
 
       const context = `[default]
-tencent_appid = ${accountId}
+tencent_appid = ${appid}
 tencent_secret_id = ${secretId}
 tencent_secret_key = ${secretKey}`.trim();
 
       await addCredentials(provider, 'credentials', context);
 
-      const dotEnvContext = `TENCENT_SECRET_ID=${secretId}
+      if (process.env['SERVERLESS_PLATFORM_VENDOR'] === 'tencent') {
+        const dotEnvContext = `TENCENT_SECRET_ID=${secretId}
 TENCENT_SECRET_KEY=${secretKey}
 SERVERLESS_PLATFORM_VENDOR=${provider}`.trim();
-      await addDotEnv(dotEnvContext);
+        await addDotEnv(dotEnvContext);
       break;
     }
-    case 'aliyuncli': {
-      const accountId = core.getInput('aliyun_account_id');
-      if (!accountId) {
-        throw new Error('Missing required arguments: ' + 'aliyun_account_id');
+    case 'aliyun': {
+      const accountId = process.env['ALICLOUD_ACCOUNT_ID'];
+      const accessKey = process.env['ALICLOUD_ACCESS_KEY'];
+      const secretKey = process.env['ALICLOUD_SECRET_KEY'];
+
+      if (!accountId || !accessKey || !secretKey) {
+        core.setFailed('Missing aliyun required environment variables');
       }
 
       const context = `[default]
 aliyun_access_key_secret = ${secretKey}
-aliyun_access_key_id = ${secretId}
+aliyun_access_key_id = ${accessKey}
 aliyun_account_id = ${accountId}`;
 
-      await addCredentials(provider, 'credentials', context);
+      await addCredentials(provider + 'cli', 'credentials', context);
       break;
     }
     default: {
@@ -93,14 +104,12 @@ export async function run() {
     }
 
     const provider = core.getInput('provider');
-    const secretId = core.getInput('secret_id');
-    const secretKey = core.getInput('secret_key');
 
-    if (!provider || !secretId || !secretKey) {
+    if (!provider) {
       core.setFailed('Missing required arguments');
     }
 
-    await useProvider(provider, secretId, secretKey);
+    await useProvider(provider);
     core.info(`Using provider ${provider}.`);
   } catch (error) {
     core.error(error);
