3f5c34cc51b524534bc02fcc61fd01b487de0226
Teakowa <hub+git@teakowa.dev>
2020-10-20 00:35:45 +0800

    refactor(main/install): use tc download instead curl

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as io from '@actions/io';
+import * as tc from '@actions/tool-cache';
 import * as utils from './utils';
 import * as credential from './credential';
 
@@ -29,40 +30,56 @@ export async function run() {
 }
 
 async function install(version: string) {
-  let output: string = '';
-  let errOutput: string = '';
+  let toolPath: string;
+  toolPath = tc.find('serverless', version);
 
-  const execOptions = {
-    listeners: {
-      stdout: (data: Buffer) => {
-        output += data.toString();
-      },
-      stderr: (data: Buffer) => {
-        errOutput += data.toString();
+  if (toolPath) {
+    await utils.info(`Found in cache @ ${toolPath}`);
+  } else {
+    let output: string = '';
+    let errOutput: string = '';
+
+    const execOptions = {
+      listeners: {
+        stdout: (data: Buffer) => {
+          output += data.toString();
+        },
+        stderr: (data: Buffer) => {
+          errOutput += data.toString();
+        }
       }
-    }
-  };
+    };
 
-  const os_version = process.platform;
+    const os_version = process.platform;
 
-  const binary_url = `https://github.com/serverless/serverless/releases/download/v${version}/serverless-${os_version}-x64`;
-  const binaries_path = `${process.env['HOME']}/.serverless/bin`;
-  const binary_path = `${binaries_path}/serverless`;
+    const binary_url = `https://github.com/serverless/serverless/releases/download/v${version}/serverless-${os_version}-x64`;
+    const binaries_path = `${process.env['HOME']}/.serverless/bin`;
+    const binary_path = `${binaries_path}/${version}/serverless`;
 
-  await utils.info(`Downloading binary...`);
-  await io.mkdirP(binary_path);
-  await exec.exec('curl ', ['-L', '-o', binary_url, binary_path]);
-  await exec.exec('sudo chmod', ['+x', binary_path]);
+    await utils.info(`Downloading binary from ${binary_url} ...`);
+    await io.mkdirP(`${binaries_path}/${version}`);
+    let downloaded_file = await tc.downloadTool(
+      binary_url,
+      binaries_path + `/serverless-${os_version}-x64`
+    );
+    await io.mv(downloaded_file, binary_path);
+    await exec.exec('ls', [`${process.env['HOME']}/.serverless/bin/`]);
+    await exec.exec('sudo chmod', ['+x', binary_path]);
 
-  await core.addPath(binaries_path);
-  await utils.info(
-    `Added by serverless binary installer\\nexport PATH=${process.env['PATH']}`
-  );
+    await core.addPath(binaries_path);
+    await utils.info(
+      `Added by serverless binary installer export PATH=${process.env['PATH']}`
+    );
 
-  await exec.exec(binary_path, ['binary-postinstall'], execOptions);
+    await exec.exec(binary_path, ['binary-postinstall'], execOptions);
 
-  return {
-    stdout: output,
-    stderr: errOutput
-  };
+    await utils.info('Adding to the cache ...');
+    await tc.cacheDir(`${binaries_path}/${version}`, 'serverless', version);
+    await utils.info('Done');
+
+    return {
+      stdout: output,
+      stderr: errOutput
+    };
+  }
 }
